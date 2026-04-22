b22c165ffa79ce3716144ccf732074c500a3f45f
CyBbuK <vadimcirkov0@gmail.com>
2026-04-22 17:02:37 +0500

    Added new folders

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import './styles/App.css';
+
+function App() {
+  return (
+    <div className="App">
+        <h1>Weather App</h1>
+        <p>Welcome to the Weather App! Please enter a city to get the current weather information.</p>
+    </div>
+  );
+}
+
+export default App;
